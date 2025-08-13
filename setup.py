2f07fbc1a1661fa73d5304f2637a6a43dcf41c85
pgshow <pgshow@gmail.com>
2025-08-13 14:06:58 +0800

    upgrade to python3.10

--- setup.py
+++ setup.py
@@ -14,15 +14,43 @@ DESCRIPTION = 'My python library package with some scraper tool and data tool'
 URL = 'https://github.com/pgshow/mylibtool'
 EMAIL = 'me@example.com'
 AUTHOR = 'Daniel'
-REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.1.6'
+REQUIRES_PYTHON = '>=3.10.0'
+VERSION = '0.2.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "addict==2.4.0", "backports.zoneinfo==0.2.1", "carehttp>=0.3.11", "certifi==2022.5.18.1", "charset-normalizer==2.0.12", "colorama==0.4.4", "date-extractor==5.1.5",
-    "dateparser==1.1.1", "filetype==1.0.13", "humanize==4.1.0", "idna>=2.8", "loguru==0.6.0", "maya==0.6.1", "pendulum==2.1.2", "python-dateutil==2.8.2", "pytz==2022.1",
-    "pytz-deprecation-shim==0.1.0.post0", "pytzdata==2020.1", "regex==2022.3.2", "requests>=2.22.0", "requests-toolbelt==0.9.1", "retrying==1.3.3", "six==1.16.0",
-    "snaptime==0.2.4", "tzdata==2022.1", "tzlocal==4.2", "urllib3>=1.25.11", "win32-setctime==1.1.0", "arrow~=1.2.2"
+    "addict==2.4.0",
+    # Py>=3.9 自带 zoneinfo：仅在老版本 Python 才需要 backports
+    'backports.zoneinfo==0.2.1; python_version < "3.9"',
+    "carehttp>=0.3.11",
+    "certifi==2022.5.18.1",
+    "charset-normalizer==2.0.12",
+    "colorama==0.4.4",
+    "date-extractor==5.1.5",
+    "dateparser==1.1.1",
+    "filetype==1.0.13",
+    "humanize==4.1.0",
+    "idna>=2.8",
+    "loguru==0.6.0",
+    # ⚠️ maya 长期未维护，Py3.10 上常见问题较多，建议尽量不再依赖
+    "maya==0.6.1",
+    "pendulum==2.1.2",
+    "python-dateutil==2.8.2",
+    "pytz==2022.1",
+    "pytz-deprecation-shim==0.1.0.post0",
+    "pytzdata==2020.1",
+    "regex==2022.3.2",
+    "requests>=2.22.0",
+    "requests-toolbelt>=1.0.0",
+    "retrying==1.3.3",
+    "six==1.16.0",
+    "snaptime==0.2.4",
+    # zoneinfo 在 Windows 往往需要 tzdata；限制到 Windows 平台安装
+    'tzdata==2022.1; platform_system == "Windows"',
+    "tzlocal==4.2",
+    "urllib3>=1.25.8,<2",
+    'win32-setctime==1.1.0; sys_platform == "win32"',
+    "arrow~=1.2.2",
 ]
 
 # What packages are optional?
@@ -79,8 +107,8 @@ class UploadCommand(Command):
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status('Building Source and Wheel distribution…')
+        os.system('{0} -m build'.format(sys.executable))
 
         self.status('Uploading the package to PyPI via Twine…')
         os.system('twine upload dist/*')
@@ -104,8 +132,9 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    py_modules=['mylibtool'],
+    # 二选一：包结构(project/包目录)用 packages；单文件模块用 py_modules。
+    # 如果你的仓库里是包目录(最常见)，请移除 py_modules：
+    # py_modules=['mylibtool'],
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
@@ -119,6 +148,8 @@ setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.10',
     ],
     # $ setup.py publish support.
     cmdclass={
